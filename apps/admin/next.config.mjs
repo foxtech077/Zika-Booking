c962fa78a74b5c5bd3dd525b80c2dd34519bff0a
akza07 <no-reply@akza.nope>
2026-05-26 20:49:46 +0530

    fix(admin): map admin route at the base of admin app

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@zika/types", "@zika/validators"],
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3001"}/:path*`,
-      },
-      {
-        source: "/listing-api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_LISTING_API_URL ?? "http://localhost:3003"}/:path*`,
-      },
-    ];
-  },
+	basePath: "/admin",
+	transpilePackages: ["@zika/types", "@zika/validators"],
+	async rewrites() {
+		return [
+			{
+				source: "/api/:path*",
+				destination: `${process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3001"}/:path*`,
+			},
+			{
+				source: "/listing-api/:path*",
+				destination: `${process.env.NEXT_PUBLIC_LISTING_API_URL ?? "http://localhost:3003"}/:path*`,
+			},
+		];
+	},
 };
 
 export default nextConfig;
